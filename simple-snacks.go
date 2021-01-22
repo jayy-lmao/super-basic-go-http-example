dd94aa4e609673d99f5489d78951736d1a9bbebc
James Holman <jameswarringtonholman@gmail.com>
2021-01-22 16:43:50 +1100

    feat: delete

--- simple-snacks.go
+++ simple-snacks.go
@@ -1,48 +1,67 @@
 package main
 
 import (
-  "net/http"
-  "strconv"
 	"github.com/webx-top/echo"
-  "github.com/webx-top/echo/engine/standard"
+	"github.com/webx-top/echo/engine/standard"
+	"net/http"
+	"strconv"
 )
 
 type (
-  Snack struct {
-    Id int `json: "snackId"`
-    Name string `json:"name" query:"name"`
-};
+	Snack struct {
+		Id   int    `json: "snackId"`
+		Name string `json:"name" query:"name"`
+	}
 )
 
 var (
 	snacks = map[int]*Snack{}
-	seq   = 1
+	seq    = 1
 )
+
+func getSnacks(c echo.Context) error {
+  return c.JSON(snacks)
+}
+
 func createSnack(c echo.Context) error {
-    // Create an ID
-    s := &Snack{
-      Id: seq,
-    }
+	// Create an ID
+	s := &Snack{
+		Id: seq,
+	}
 
-    // Add other properties
-    if err := c.Bind(s);err!=nil{
-      return err
-    }
+	// Add other properties
+	if err := c.Bind(s); err != nil {
+		return err
+	}
 
-    snacks[s.Id] = s
-    seq++
+	snacks[s.Id] = s
+	seq++
 
-    return  c.JSON(s,http.StatusCreated)
+	return c.JSON(s, http.StatusCreated)
+}
+
+func deleteSnack(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	delete(snacks, id)
+	if err != nil {
+		return c.JSON(err, http.StatusBadRequest)
+	}
+	return c.NoContent(http.StatusNoContent)
 }
 
 func getSnack(c echo.Context) error {
-  id, err := strconv.Atoi(c.Param("id"))
-  if err != nil {
-    return c.JSON(err, http.StatusBadRequest)
-  }
-  return c.JSON(snacks[id], http.StatusOK)
-}
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(err, http.StatusBadRequest)
+	}
+  snack := snacks[id]
 
+  if snack == nil {
+    return c.NoContent(http.StatusNotFound)
+  }
+
+	return c.JSON(snacks[id], http.StatusOK)
+}
 
 func main() {
 	e := echo.New()
@@ -50,8 +69,10 @@ func main() {
 	e.Get("/", func(c echo.Context) error {
 		return c.String("Hello, World!", http.StatusOK)
 	})
-  e.Get("/snacks/:id", getSnack)
-  e.Post("/snacks", createSnack)
+	e.Get("/snacks", getSnacks)
+	e.Get("/snacks/:id", getSnack)
+	e.Delete("/snacks/:id", deleteSnack)
+	e.Post("/snacks", createSnack)
 
-  e.Run(standard.New(":1323"));
+	e.Run(standard.New(":1323"))
 }
